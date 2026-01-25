91e344906b6b2fecd0e3eb49789ac1cf71752123
Felix Carrier <carrier.felix@outlook.com>
2026-01-25 18:28:38 -0500

    Setup eslint, prettier and format on save

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,12 @@
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
+import prettier from 'eslint-config-prettier'
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+  prettier,
+  {
+    rules: {},
+  },
+]
