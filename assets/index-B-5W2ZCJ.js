09e9fcd6830660ca8ceff43212448ea0d7455893
Felix Carrier <carrier.felix@outlook.com>
2026-02-16 23:08:13 -0500

    Updates

---
+++ assets/index-B-5W2ZCJ.js
@@ -0,0 +1,10 @@
+import{j as _}from"./index-DDjV0QQT.js";const e=`
+  _______ _____ _____             _                    _____                  _ _        
+ |__   __/ ____|  __ \\           | |                  / ____|                (_) |       
+    | | | (___ | |__) |___  _   _| |_ ___ _ __ ______| |     ___   __ _ _ __  _| |_ ___  
+    | |  \\___ \\|  _  // _ \\| | | | __/ _ \\ '__|______| |    / _ \\ / _\` | '_ \\| | __/ _ \\ 
+    | |  ____) | | \\ \\ (_) | |_| | ||  __/ |         | |___| (_) | (_| | | | | | || (_) |
+    |_| |_____/|_|  \\_\\___/ \\__,_|\\__\\___|_|          \\_____\\___/ \\__, |_| |_|_|\\__\\___/ 
+                                                                   __/ |                 
+                                                                  |___/                  
+`;function l(){return _.jsx("div",{className:"h-full flex flex-col",children:_.jsx("header",{className:"flex-1 flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]",children:_.jsx("pre",{className:"overflow-x-auto font-mono text-sm",children:e})})})}export{l as component};
